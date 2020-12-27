8e97610c211f8dd2b7b4134040c7ceecdcf65801
Michael Fuget <michaelfuget@gmail.com>
2020-12-27 00:40:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,45 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  configure :development do
+    set :database, 'sqlite3:db/articles.db'
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
+  end
+  get '/articles/:id' do
+    @article = Article.find(params[:id]) 
+    erb :show
+  end
+
+  
+  post '/articles' do 
+    new_article = Article.create(params[:article])
+    redirect "/articles/#{new_article.id}"
+  end
+  
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do 
+    edit_article = Article.find(params[:id])
+    edit_article.update(params[:article])
+    redirect "/articles/#{edit_article.id}"
+
+  end
+  
+  delete '/articles/:id' do 
+    delete_article = Article.find(params[:id])
+    delete_article.destroy
+
   end
 end
